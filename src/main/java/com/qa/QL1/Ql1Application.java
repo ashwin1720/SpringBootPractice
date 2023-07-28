741e705bccfed35333a27bfc36f59a15bb94474b
Ashwin <Ashwinsuresh2013@gmail.com>
2023-07-28 18:54:13 +0100

    Halway ql4

--- src/main/java/com/qa/QL1/Ql1Application.java
+++ src/main/java/com/qa/QL1/Ql1Application.java
@@ -1,13 +1,37 @@
 package com.qa.QL1;
 
+import com.qa.QL1.model.Hero;
+import com.qa.QL1.practice.Calculator;
+//import com.qa.QL1.repository.HeroRepositor;
+//import com.qa.QL1.repository.HeroRepository;
+import com.qa.QL1.repository.HeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Ql1Application {
+import java.util.List;
 
+@SpringBootApplication
+public class Ql1Application implements CommandLineRunner {
+
+	@Autowired
+	Calculator calc;
+	@Autowired
+	HeroRepository heroRepository;
 	public static void main(String[] args) {
+
 		SpringApplication.run(Ql1Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+		int result = calc.addition(3,4);
+		System.out.println(result);
+		List<Hero> heroes = heroRepository.findAll();
+		for (Hero hero: heroes) {
+			System.out.println(hero);
+		}
+	}
 }
---
+++ src/main/java/com/qa/QL1/practice/Calculator.java
@@ -0,0 +1,8 @@
+package com.qa.QL1.practice;
+import org.springframework.stereotype.Component;
+@Component
+public class Calculator {
+    public int addition(int num1, int num2){
+        return num1+num2;
+    }
+}
