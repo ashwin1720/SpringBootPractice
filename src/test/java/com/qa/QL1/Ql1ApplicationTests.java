34e1b43f30b7d01d0f85b7132ac7c0bc4675db27
Ashwin <Ashwinsuresh2013@gmail.com>
2023-07-28 15:04:57 +0100

    Initial Commit For QL1

---
+++ src/test/java/com/qa/QL1/Ql1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.QL1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ql1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
